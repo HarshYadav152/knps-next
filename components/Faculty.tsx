83b1d66519423b9e50189252dd8dfc58d027a0c8
Harsh Yadav <161936085+HarshYadav152@users.noreply.github.com>
2026-03-25 21:39:39 +0530

    update faculty secion

--- components/Faculty.tsx
+++ components/Faculty.tsx
@@ -69,7 +69,7 @@ const Faculty: React.FC = () => {
             Meet Our {' '}
             <span className="relative inline-block">
               <span className="relative z-10 text-gold-400">Faculty</span>
-              <span className="absolute bottom-2 left-0 w-full h-3 bg-gold-500/20 -z-0 rounded-full"></span>
+              {/* <span className="absolute bottom-2 left-0 w-full h-12 bg-gold-500/20 -z-0 rounded"></span> */}
             </span>
           </h2>
           
